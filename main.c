871c229b8f8267aab2c4eff5e7c814a67f9f4f90
Alexander Livenets <a.livenets@gmail.com>
2017-02-18 20:46:09 +0700

    Refactor function signatures

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
-size_t pack7bit(const char *str, uint8_t *out, size_t out_len)
+bool pack7bit(const char *str, uint8_t *const out, size_t *const out_len)
 {
     const size_t len = strlen(str);
     size_t pack_len = 0;
     uint8_t carryBitsCount = 0;
     uint8_t carryBits = 0;
+    const size_t max_out_len = *out_len;
+
+    if (len < ((max_out_len * 7) / 8))
+        return false;
 
     for(size_t i = 0; i < len; ++i) {
         uint8_t byte = (str[i] & 0x7F);
@@ -86,9 +91,6 @@ size_t pack7bit(const char *str, uint8_t *out, size_t out_len)
             out[pack_len] = carryBits;
             ++pack_len;
 
-            if (pack_len >= out_len)
-                break;
-
             carryBitsCount = 0;
 
             if (toPush > 0) {
@@ -142,22 +144,27 @@ size_t pack7bit(const char *str, uint8_t *out, size_t out_len)
     }
 
     if (carryBitsCount > 0) {
-        if (pack_len < out_len)
-            out[pack_len] = carryBits;
-
+        out[pack_len] = carryBits;
         ++pack_len;
     }
 
-    return pack_len;
+    *out_len = pack_len;
+
+    return true;
 }
 
-size_t unpack7bit(const uint8_t *str7bit, size_t in_len, char *out, size_t out_len)
+bool unpack7bit(const uint8_t *str7bit, size_t len, char *out, size_t *out_len)
 {
     size_t unpack_len = 0;
     uint8_t carryBitsCount = 0;
     uint8_t carryBits = 0;
 
-    for (size_t i = 0; i < in_len; ++i) {
+    size_t max_out_len = *out_len;
+
+    if (max_out_len < ((len * 8)/ 7))
+        return false;
+
+    for (size_t i = 0; i < len; ++i) {
         const uint8_t byte = str7bit[i];
         uint8_t toPush = 0;
 
@@ -225,79 +232,72 @@ size_t unpack7bit(const uint8_t *str7bit, size_t in_len, char *out, size_t out_l
         out[unpack_len] = (carryBits & 0x7F);
         ++unpack_len;
 
-        if (unpack_len >= out_len)
-            break;
-
         carryBits >>= 7;
         carryBitsCount -= 7;
 
         if (toPush > 0) {
-            if (unpack_len < out_len) {
-                switch (toPush) {
-                    case 0:
+            switch (toPush) {
+                case 0:
 //                      carryBits = byte >> 0;
-                        carryBitsCount += 0;
-                        break;
-                    case 1:
-                        carryBits |= (byte >> 7) << 1;
-                        carryBitsCount += 1;
-                        break;
-                    case 2:
-                        carryBits |= (byte >> 6) << 1;
-                        carryBitsCount += 2;
-                        break;
-                    case 3:
-                        carryBits |= (byte >> 5) << 1;
-                        carryBitsCount += 3;
-                        break;
-                    case 4:
-                        carryBits |= (byte >> 4) << 1;
-                        carryBitsCount += 4;
-                        break;
-                    case 5:
-                        carryBits |= (byte >> 3) << 1;
-                        carryBitsCount += 5;
-                        break;
-                    case 6:
-                        carryBits |= (byte >> 2) << 1;
-                        carryBitsCount += 6;
-                        break;
-                    case 7:
-                        carryBits |= (byte >> 1) << 1;
-                        carryBitsCount += 7;
-                        break;
-                    case 8:
-                        carryBits |= (byte >> 0) << 1;
-                        out[unpack_len] = (carryBits & 0x7F);
-                        ++unpack_len;
-                        carryBits = carryBits >> 7;
-                        carryBits |= (byte >> 7) << 1;
-                        carryBitsCount = 2;
+                    carryBitsCount += 0;
+                    break;
+                case 1:
+                    carryBits |= (byte >> 7) << 1;
+                    carryBitsCount += 1;
+                    break;
+                case 2:
+                    carryBits |= (byte >> 6) << 1;
+                    carryBitsCount += 2;
+                    break;
+                case 3:
+                    carryBits |= (byte >> 5) << 1;
+                    carryBitsCount += 3;
+                    break;
+                case 4:
+                    carryBits |= (byte >> 4) << 1;
+                    carryBitsCount += 4;
+                    break;
+                case 5:
+                    carryBits |= (byte >> 3) << 1;
+                    carryBitsCount += 5;
+                    break;
+                case 6:
+                    carryBits |= (byte >> 2) << 1;
+                    carryBitsCount += 6;
+                    break;
+                case 7:
+                    carryBits |= (byte >> 1) << 1;
+                    carryBitsCount += 7;
+                    break;
+                case 8:
+                    carryBits |= (byte >> 0) << 1;
+                    out[unpack_len] = (carryBits & 0x7F);
+                    ++unpack_len;
+                    carryBits = carryBits >> 7;
+                    carryBits |= (byte >> 7) << 1;
+                    carryBitsCount = 2;
 
-                        break;
-                    default:
-                        assert(0);
-                        break;
-
-                }
+                    break;
+                default:
+                    assert(0);
+                    break;
             }
         }
     }
 
     if (carryBitsCount > 0) {
         if (carryBitsCount == 8) {
-            if (unpack_len < out_len)
-                out[unpack_len] = (carryBits & 0x7F);
-            if (unpack_len+1 < out_len)
-                out[unpack_len+1] = (carryBits >> 7) & 0x01;
+            out[unpack_len] = (carryBits & 0x7F);
+            out[unpack_len+1] = (carryBits >> 7) & 0x01;
         }
         else {
-            if (unpack_len < out_len)
-                out[unpack_len] = carryBits & 0x7F;
+            out[unpack_len] = carryBits & 0x7F;
         }
     }
 
-    return unpack_len;
+    *out_len = unpack_len;
+
+    return true;
 }
 
 void dumphex(const uint8_t *buf, size_t len)
@@ -323,21 +323,22 @@ int main(int argc, char *argv[])
     printf("Input HEX:\n");
     dumphex((const uint8_t*)str, sizeof(str));
 
-    size_t len;
-    len = pack7bit(str, buf, sizeof(buf));
+    size_t pack_len = sizeof(buf);
+    bool ret = pack7bit(str, buf, &pack_len);
 
-    printf("Pack7:\n");
+    printf("Pack7 (ret = %d):\n", ret);
     dumphex(buf, sizeof(buf));
 
-    len = unpack7bit(buf, sizeof(buf), unpack_buf, sizeof(unpack_buf));
+    size_t unpack_len = sizeof(unpack_buf);
+    ret = unpack7bit(buf, pack_len, unpack_buf, &unpack_len);
 
-    printf("Unpack7 HEX:\n");
-    dumphex(unpack_buf, sizeof(unpack_buf));
+    printf("Unpack7 HEX (ret = %d):\n", ret);
+    dumphex((const uint8_t*)unpack_buf, sizeof(unpack_buf));
 
     printf("Unpack7: %s\n", unpack_buf);
 
     if(strcmp(str, unpack_buf) == 0) {
-        printf("--OK--\n");
+        printf("--- OK ---\n");
     }
     else {
         printf("!!! CMP_FAIL !!!\n");
